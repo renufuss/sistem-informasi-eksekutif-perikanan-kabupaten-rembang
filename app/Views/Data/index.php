43a060d6b1797eddbecfaf8a28640bbabc65d2cd
Renanda <renanda0039934@gmail.com>
2023-12-08 09:27:44 +0700

    feat: add view for data menu

--- app/Views/Data/index.php
+++ app/Views/Data/index.php
@@ -1,6 +1,44 @@
 <?= $this->extend('Layouts/index'); ?>
 
 <?= $this->section('content'); ?>
-
-
+<div class="row">
+    <div class="col-12">
+        <div class="card shadow-sm">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-striped gy-7 gs-7">
+                        <thead>
+                            <tr class="fw-semibold fs-6 text-gray-800 border-bottom-2 border-gray-200">
+                                <th class="min-w-200px">Name</th>
+                                <th class="min-w-400px">Position</th>
+                                <th class="min-w-100px">Office</th>
+                                <th class="min-w-200px">Age</th>
+                                <th class="min-w-200px">Start date</th>
+                                <th class="min-w-200px">Salary</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Tiger Nixon</td>
+                                <td>System Architect</td>
+                                <td>Edinburgh</td>
+                                <td>61</td>
+                                <td>2011/04/25</td>
+                                <td>$320,800</td>
+                            </tr>
+                            <tr>
+                                <td>Garrett Winters</td>
+                                <td>Accountant</td>
+                                <td>Tokyo</td>
+                                <td>63</td>
+                                <td>2011/07/25</td>
+                                <td>$170,750</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?= $this->endSection(); ?>
